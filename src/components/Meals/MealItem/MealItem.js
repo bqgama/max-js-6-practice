44b3f7108c538c4b9b59e1b36008726943a728ad
bqgama <bqgama@gmail.com>
2023-03-10 07:38:24 -0300

    created form and cart components

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,3 +1,4 @@
+import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
 
 const MealItem = (props) => {
@@ -7,10 +8,10 @@ const MealItem = (props) => {
       <div>
         <h3>{props.name}</h3>
         <div className={classes.description}>{props.description}</div>
-        <div className={classes.price}>{props.price}</div>
+        <div className={classes.price}>{price}</div>
       </div>
       <div>
-
+        <MealItemForm id={props.id} />
       </div>
     </li>
   );
